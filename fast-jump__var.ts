042d1b7c69663d4002e1026687d8473d992932fd
Simon He <57086651+Simon-He95@users.noreply.github.com>
2024-03-06 13:42:14 +0800

    chore: update

--- fast-jump__var.ts
+++
@@ -1,6 +0,0 @@
-export type VueAst = AST<{ jsx: true; loc: true }>
-type AST<T extends TSESTreeOptions> = TSESTree.Program & (T['comment'] extends true ? {
-  comments: TSESTree.Comment[]
-} : EmptyObject) & (T['tokens'] extends true ? {
-  tokens: TSESTree.Token[]
-} : EmptyObject)
